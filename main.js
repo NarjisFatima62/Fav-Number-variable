ffca23e84cff543e65369a601fc2d4a6ae4a4f35
Narjis Fatima <161502591+NarjisFatima62@users.noreply.github.com>
2024-05-11 20:56:07 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+// Question Number: 09
+//Favorite Number: Store your favorite number in avariable. Then using that variable, create a message that reveals your favorite number. Print that message.  
+var favoriteNumber = 10;
+console.log("My favorit number is ".concat(favoriteNumber, ","));
---
+++ main.ts
@@ -0,0 +1,5 @@
+// Question Number: 09
+//Favorite Number: Store your favorite number in avariable. Then using that variable, create a message that reveals your favorite number. Print that message.  
+
+let favoriteNumber: number = 10;
+console.log(`My favorit number is ${favoriteNumber},`);
